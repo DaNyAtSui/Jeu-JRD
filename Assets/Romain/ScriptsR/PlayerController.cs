80dbc603a78d7baa5f74d665a223ec8c12492db2
romaingangama <romaingangama@gmail.com>
2025-10-29 14:27:00 +0400

    save

---
+++ Assets/Romain/ScriptsR/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float movSpeed;
+    float speedX, speedY;
+    Rigidbody2D rb;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void Update()
+    {
+        speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
+        rb.linearVelocity = new Vector2(speedX, speedY);
+    }
+}
